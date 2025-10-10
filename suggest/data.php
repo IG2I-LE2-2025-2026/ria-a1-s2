42f987b3cd60beb8d33ec2133a00d17d25f293b8
Maxime Folschette <Maxime.Folschette@centralelille.fr>
2025-10-10 17:23:58 +0200

    fichier data pour V3

---
+++ suggest/data.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_GET["debutNom"])) 
+{
+	$cherche = $_GET["debutNom"]; 
+	
+	// On va ouvrir un fichier et afficher les lignes 
+	// où le prénom ou le nom contient ce texte
+
+	$tabLignes = file("LE2_2025_2026.csv");
+	foreach ($tabLignes as $ligne)
+	{
+		// TODO EXO1 : effectuer une recherche sur nom ou prénom 
+		if (preg_match("/^.*:(" . $cherche . ".*)$/i",$ligne,$tabResultats))
+		{
+			// TODO EXO2 afficher nom ET prénom 
+			echo $tabResultats[1]; 
+			// TODO formater en XHTML
+		}
+	}
+
+	die("");
+}
+
+?>
