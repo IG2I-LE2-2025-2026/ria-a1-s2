82becafb91c698baeeead9ec8b6b567ed444c550
Maxime Folschette <Maxime.Folschette@centralelille.fr>
2025-10-10 17:08:36 +0200

    suggest V2

--- suggest/js/suggest.js
+++ suggest/js/suggest.js
@@ -1,3 +1,15 @@
 // Fonctions pour le suggest
 
+function recherche_suggestions() {
+  console.log("Fonction recherche_suggestions");
+  var recherche = val("recherche");
+  // TODO : requête AJAX
+}
+
+function valider_suggestion(e) {
+  console.log("Fonction valider_suggestion");
+  console.log(e);
+  // TODO : intégrer la suggestion cliquée dans le champ de texte
+  val("recherche", html(e.target).trim());
+}
 
