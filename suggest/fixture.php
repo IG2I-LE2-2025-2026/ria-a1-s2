186bd5cbea2f2858d033c25f614c68967cefb9c8
Maxime Folschette <Maxime.Folschette@centralelille.fr>
2025-10-10 17:08:49 +0200

    fixture pour V3

---
+++ suggest/fixture.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '
+  <div class="suggestion">
+    Maxime Folschette
+  </div>
+  <div class="suggestion">
+    Thomas Bourdeaud\'huy
+  </div>
+  <div class="suggestion">
+    Jean-Jacques Martin
+  </div>
+  <div class="suggestion">
+    Test Test
+  </div>
+  <div class="suggestion">' .
+    rand()
+  . '</div>';
+
+?>
