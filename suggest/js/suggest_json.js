310df1915b67fac65effb922433fa7eea22d6b1e
tb59 <thomas.bourdeaudhuy@gmail.com>
2025-10-22 09:18:32 +0200

    correction suggest V4

--- suggest/js/suggest_json.js
+++ suggest/js/suggest_json.js
@@ -3,15 +3,36 @@
 function recherche_suggestions() {
   console.log("Fonction recherche_suggestions");
   var recherche = val("recherche");
-  // TODO : requête AJAX
+  // TODO : 1) transformer le résultat (chaine) en objet JSON
+  // 2) parcourir l'objet pour créer le XHTML
+  // 3) insérer le XHTML dans le champ "suggest"
   ajax({
-    url: "data.php",
+    url: "data_json.php",
     donnees : {
     	debutNom : recherche
     }, 
-    callback: function (r) {
+    callback: function (chaineJson) {
+    	var objetJson = JSON.parse(chaineJson);
+    	//console.log(chaineJson); 
+    	//console.log(objetJson); 
+    	var suggestXhtml = ""; 
+    	/*
+    	{"annee":2025,"promo":"LE2",
+    		"etudiants":[{"prenom":"Mohamed Othman","nom":"BAZEGA","id":"2"}, ... ]}
+    	*/
+    	// objetJson.etudiants est un tableau 
+    	// sa taille : objetJson.etudiants.length
+    	var i; 
+    	for(i=0;i<objetJson.etudiants.length;i++) {
+    		suggestXhtml += "<div class=\"suggestion\">"; 
+    		suggestXhtml += objetJson.etudiants[i].prenom.substring(0,1);
+  		 	suggestXhtml += ". ";
+		 	  suggestXhtml += objetJson.etudiants[i].nom;
+    		suggestXhtml += "</div>"; 
+    		
+    	}
       console.log("Retour AAJX recherche_suggestions");
-      html("suggest", r);
+      html("suggest", suggestXhtml);
     }
   });
 }
